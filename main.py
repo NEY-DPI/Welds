58ebd732673101feb79656c5a4fff43fad7b9f7d
Denis Piron <dpi@ney.partners>
2023-04-21 14:44:04 +0200

    test

--- main.py
+++ main.py
@@ -9,9 +9,33 @@ from io import StringIO
 '''
 
 "## Import Files"
-"### Eigenfrequencies"
+"### Import txt file from wingraf"
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+    data = uploaded_file.readlines()
 
+    appending = False
+    list = []
+    results = []
+
+    for line in data:
+        if not appending:
+            if "no." in line:
+                list = []
+                appending = True
+        else:
+            if len(line.split()) < 1:
+                appending = False
+                results.append(list)
+            else:
+                list.append(line)
+
+    results1 = []
+    for list in results:
+        list1 = []
+        for line in list:
+            list1.append([float(x) for x in line.split()])
+        results1.append(list1)
+
+
+results1
