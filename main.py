2dbbeae779c1ad62bbb2dd0e403d84bd25acb463
Denis Piron <dpi@ney.partners>
2023-04-21 14:24:13 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+from io import StringIO
+
+'''
+# Footbridge-Vibrations
+
+
+'''
+
+"## Import Files"
+"### Eigenfrequencies"
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
+
